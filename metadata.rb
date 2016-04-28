be7e25ebf30aa44819c6e1d8b687f42e76b3890c
Ian Henry <ihenry@chef.io>
2016-04-28 11:17:20 -0400

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'confd-cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures confd-cookbook'
+long_description 'Installs/Configures confd-cookbook'
+version '0.1.0'
